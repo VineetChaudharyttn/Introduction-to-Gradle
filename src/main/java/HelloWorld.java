1437e02b1d62c421bd613ab30ef6bd2a0d696b63
VineetChaudharyttn <vineet.chaudhary@tothenew.com>
2019-03-06 17:01:31 +0530

    complete

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+import org.apache.commons.lang3.*;
+class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println(StringUtils.rotate("HelloWorld",5));
+
+        new Second().show();
+    }
+}
---
+++ src/main/java2/Second.java
@@ -0,0 +1,5 @@
+public class Second {
+    void show(){
+        System.out.println("Second file is invoked");
+    }
+}
